e4e6c1a78f7f902ad87baec3ad609a2b5e3684ae
yaakov lombard <79059545+yaakovlom@users.noreply.github.com>
2023-12-18 16:22:43 +0200

    Update content.js
    remove logs and comments

--- ad_skip_extension/content.js
+++ ad_skip_extension/content.js
@@ -1,8 +1,6 @@
-console.log("start ")
 var skipAdElement = document.querySelector("div.skipad.hidden");
 if (skipAdElement) {
     skipAdElement.click()
-    // skipAdElement.classList.remove('hidden');
 }
 else {
 console.log("cannot find element", skipAdElement)
