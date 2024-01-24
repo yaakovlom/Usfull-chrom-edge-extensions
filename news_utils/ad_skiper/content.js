a0648353cb858caa5ed5be8e3534be374c589f90
yaakov lombard <79059545+yaakovlom@users.noreply.github.com>
2024-01-24 12:40:30 +0200

    fix directories name + ad_skiper for jdn

---
+++ news_utils/ad_skiper/content.js
@@ -0,0 +1,22 @@
+var currentUrl = window.location.href;
+var elementToClick;
+
+function clickOnElement(elementToClick) {
+    if (elementToClick.click) {
+        elementToClick.click();
+    }
+}
+
+if (currentUrl.includes("kore.co.il")) {
+    for (elementToClick of document.querySelectorAll("div.skipad.hidden")) {
+        if (elementToClick && ! elementToClick.parentElement.firstChild.firstElementChild.firstElementChild.firstElementChild.classList.contains("fa-play")) {
+            clickOnElement(elementToClick);
+        }
+    }
+} else if (currentUrl.includes("jdn.co.il")) {
+    for (elementToClick of document.querySelectorAll("a.undefined.is-active")) {
+        if (elementToClick && ! elementToClick.parentElement.parentElement.parentElement.parentElement.classList.contains("last-video")) {
+            clickOnElement(elementToClick.parentElement.lastChild);
+            }
+    }
+}
